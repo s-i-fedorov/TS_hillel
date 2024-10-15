33d7ca612eaa4fdaf4949e7b9cf564fd8d715c42
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-10-16 00:19:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'
+const BankAccount = ()=>{
+    const deposit = (a)=>{
+        return a;
+    }
+    const withdraw = (b)=>{
+        return b;
+    }
+    const getBalance = (c)=>{
+        return c;
+    }
+
+}
