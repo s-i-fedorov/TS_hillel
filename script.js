3ff1fb7f1e5c94e16eef0b7494be8b7453d8a84c
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-10-16 22:43:38 +0300

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 'use strict'
-const BankAccount = ()=>{
-    const deposit = (a)=>{
-        return a;
+
+function BankAccount(name,age,phone){
+    let currentBalance = 0;
+    this.abName = name;
+    this.age = age;
+    this.phone = phone;
+
+    this.deposit = function (sum){
+        currentBalance = currentBalance + sum;
     }
-    const withdraw = (b)=>{
-        return b;
+
+    this.withdraw = function (sum){
+        currentBalance = currentBalance - sum;
     }
-    const getBalance = (c)=>{
-        return c;
+
+    this.getBalance = function (){
+        return currentBalance;
     }
 
 }
+let user = new BankAccount('Serhii',36, '0931111111');
+
+
+
+user.deposit(10)
+user.withdraw(1)
+user.deposit(3)
+console.log(user.getBalance())
+
+
+
+
