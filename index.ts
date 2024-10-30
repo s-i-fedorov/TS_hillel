0286ff524a315bb09d20f5b2d7be6c0867e0893b
s-i-fedorov <145291611+s-i-fedorov@users.noreply.github.com>
2024-10-30 18:16:39 +0200

    commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+function parsePrice(price) {
+    const priseCopy = price
+    let outInfo = []
+    for (const key in priseCopy) {
+        if (Object.prototype.hasOwnProperty.call(priseCopy, key)) {
+            const element = priseCopy[key];
+            
+        }
+    }
+}
+
+const myObj = {
+    pos1: {
+        name: 'phone',
+        price: '400',
+        isAvailable: true
+    },
+    pos2: {
+        name: 'tablet',
+        price: '600',
+        isAvailable: true
+    },
+    pos3: {
+        name: 'laptop',
+        price: '1000',
+        isAvailable: true
+    },
+    pos4: {
+        name: 'TV',
+        price: '500',
+        isAvailable: true
+    },
+    pos5: {
+        name: 'clock',
+        price: '200',
+        isAvailable: true
+    },
+    
+}
--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict'
+const myObj = {
+    pos1: {
+        name: 'phone',
+        price: '400',
+        isAvailable: true
+    },
+    pos2: {
+        name: 'tablet',
+        price: '600',
+        isAvailable: true
+    },
+    pos3: {
+        name: 'laptop',
+        price: '1000',
+        isAvailable: true
+    },
+    pos4: {
+        name: 'TV',
+        price: '500',
+        isAvailable: true
+    },
+    pos5: {
+        name: 'clock',
+        price: '200',
+        isAvailable: true
+    },
+    
+}
+console.log(myObj);
