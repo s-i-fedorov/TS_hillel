70c8a908eac2838880cf5d26fb4d56689f460bcf
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-04 23:03:48 +0200

    hw_2_it works

--- index.ts
+++ index.ts
@@ -1,23 +1,29 @@
-const prod1 = {
+type Product = {
+    name: string;
+    price: number;
+    isAvailable: boolean
+}
+const prod1:Product = {
         name: 'phone',
-        price: '400',
+        price: 400,
         isAvailable: true
     }
-const prod2 = {
+const prod2:Product = {
         name: 'tablet',
-        price: '600',
+        price: 600,
         isAvailable: true
     }
-const prod3 = {
+const prod3:Product = {
         name: 'laptop',
-        price: '1000',
-        isAvailable: true
+        price: 1000,
+        isAvailable: false
     }
 
-function parsePrice(obj):void {
+function parsePrice(obj:Product):void {
     const name:string = obj.name
     const price:number = obj.price
     const isAvailable:boolean = obj.isAvailable
     let available: string = isAvailable ? 'Yes' : 'No'
     console.log(`Товар: ${name}, Ціна: ${price}$, В наявності: ${available}`)
 }
+parsePrice(prod3)
