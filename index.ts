ac3b26fbc1eb2bfc304789198c262b024a9634a5
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-06 22:23:35 +0200

    corrected

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 type SimpleArray = (string|number)[]
 const array:SimpleArray = [1,2,3,4,5,6,7,8,9,10]
 function reverseArr(arr:SimpleArray): SimpleArray{
+    if (arr.length === 0) console.log('Error, array is empty')
     let temp:number|string = 0
     let start:number = 0
     let end:number = arr.length-1
