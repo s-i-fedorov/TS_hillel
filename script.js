1c47174f05a4e73cefd293d2d574c3ddf817c4df
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-11 10:58:21 +0200

    1 commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict'
+const text = "This is a test sentence with some bad words.";
+const forbiddenWords = ["bad", "test"];
+// const result = highlightForbiddenWords(text, forbiddenWords);
+// console.log(result); // "This is a <del>test</del> sentence with some <del>bad</del> words."
+
+function highlightForbiddenWords (text, wordsArray){
+    return text.split(' ')
+}
+
+console.log(highlightForbiddenWords(text))
