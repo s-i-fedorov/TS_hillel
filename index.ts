2f368f46fd4282d429d0d4dffa865069800b173c
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-12 21:50:52 +0200

    done

--- index.ts
+++ index.ts
@@ -5,24 +5,38 @@ function highlightForbiddenWords (text:string, wordsArray:string[]){
     const textArr = text.split(' ')
     return textArr.map(item => wordsArray.includes(item)?`<del>${item}</del>`:item).join(' ')
 }
-function highlightForbiddenWords2 (){
-    const wordsArray:string[] = forbiddenWords
-    const text:string = document.querySelector('[data-sentence]').innerHTML
+function highlightForbiddenWords2 (wordsArray:string[]):void{
+    const element:HTMLElement | null = document.querySelector('[data-sentence]')
+    if (!element) {
+        console.error("Елемент з data-sentence не знайдено.");
+        return;
+    }
+    const text:string = element.innerHTML
     const textArr:string[] = text.split(' ')
-    document.querySelector('[data-sentence]').innerHTML =
+    element.innerHTML =
         textArr.map(item => wordsArray.includes(item)?`<del>${item}</del>`:item).join(' ')
 }
 
 // const result = highlightForbiddenWords(text, forbiddenWords);
 // console.log(result); // "This is a <del>test</del> sentence with some <del>bad</del> words."
 // console.log(document.querySelector('[data-sentence]').innerHTML)
-function clickHandler(e){
+function clickHandler(e:MouseEvent):void{
     const {target} = e;
-    if (target.hasAttribute('data-btn1')) {
-        highlightForbiddenWords2()
+    if (!(target instanceof HTMLElement)){
+        console.error('Error')
+        return;
+    }
+    else if (target.hasAttribute('data-btn1')) {
+        highlightForbiddenWords2(forbiddenWords)
     }
     else if (target.hasAttribute('data-btn2')) {
-        document.querySelector('[data-sentence]').innerHTML = text
+        const element:HTMLElement|null=document.querySelector('[data-sentence]')
+        if(!element){
+            console.error('Елемент не знайдено')
+            return
+        }
+        element.innerHTML = text
     }
 }
 document.body.addEventListener("click", clickHandler)
+
