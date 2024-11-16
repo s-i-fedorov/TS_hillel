8ec4aaf41af313f3ccceacd269bc32bd05c81c86
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-16 22:25:01 +0200

    1

--- index.ts
+++ index.ts
@@ -3,15 +3,22 @@ function deepClone(obj:{}|[]):{}{
         return obj
     }
     if (Array.isArray(obj)){
+        if(obj.length===0) return []
         let arrCopy:[] = []
         obj.forEach(item=>arrCopy.push(item))
         return arrCopy
     }
     const objCopy = {}
     for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
+
             objCopy[key] = deepClone(obj[key]);
-        }
+
     }
     return objCopy
 }
+
+const original = { a: 1, b: { c: 2 }, d: [3, 4] };
+// const copy = deepClone(original);
+// original !== copy
+// original.b !== copy.b
+// original.d !== copy.d
