7cd63c5775905328a8656546b65ad570ccace2c7
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-17 22:53:09 +0200

    with errors

--- index.ts
+++ index.ts
@@ -17,9 +17,6 @@ function deepClone(obj:Example): {}|[]{
 
 interface Example {
     [key:string]:string|number|number[]|string[]|{}
-    // a: number,
-    // b: {c:number},
-    // d: number[]
 }
 
 const original:Example = { a: 1, b: { c: 2 }, d: [3, 4] };
