e108816504e361a8d8b44c66440183d9d2f762bf
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-24 21:26:28 +0200

    done

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-interface Student {
-    id: string,
-    name: string,
-    age: number
-}
-
-type Subject = 'Math' | 'Science' | 'Literature' | 'History'
-
-type Grades = Record<Subject, number>
-
-interface UniversityRecord {
-    students: Record<string, Student>,
-    grades: Record<string, Grades>
-}
-
-const universityRecord: UniversityRecord = {
+"use strict";
+const universityRecord = {
     students: {
         "s1": { id: "1", name: "Alex", age: 19 },
         "s2": { id: "2", name: "Evgeniy", age: 20 },
@@ -27,21 +13,21 @@ const universityRecord: UniversityRecord = {
 };
 //     Написати функцію getStudentGrades, яка приймає universityRecord: UniversityRecord та studentId: string,
 //     і повертає оцінки студента за предметами.
-function getStudentGrades (universityRecord: UniversityRecord, studentId: string): Grades|void {
+function getStudentGrades(universityRecord, studentId) {
     const findKey = Object.keys(universityRecord.students).
-    find(item=>universityRecord.students[item].id === studentId)
-    if (!findKey) return console.error('student was not find')
-    return universityRecord.grades[findKey]
+        find(item => universityRecord.students[item].id === studentId);
+    if (!findKey)
+        return console.error('student was not find');
+    return universityRecord.grades[findKey];
 }
 //     Написати функцію getAverageGrade, яка приймає universityRecord: UniversityRecord та subject: Subject,
 //     і повертає середню оцінку всіх студентів по зазначеному предмету.
-function getAverageGrade (universityRecord: UniversityRecord, subject: Subject): number {
+function getAverageGrade(universityRecord, subject) {
     const valuesArr = Object.values(universityRecord.grades);
-    const result = valuesArr.reduce((acc,item)=>{
-        return acc + item[subject]
-    },0)
-    return result/valuesArr.length
+    const result = valuesArr.reduce((acc, item) => {
+        return acc + item[subject];
+    }, 0);
+    return result / valuesArr.length;
 }
-
 console.log(getStudentGrades(universityRecord, '3'));
-console.log(getAverageGrade(universityRecord, 'Math'));
+getAverageGrade(universityRecord, 'Math');
