ad492146a247f4b053d95de6bf29606900c02dcb
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-11-26 22:22:35 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict'
+// Створи клас User, який має:
+//
+//     Властивості:
+//         id (унікальний ідентифікатор, автоматично генерується для кожного екземпляра);
+// name (ім’я);
+// email (електронна пошта);
+// приватну властивість password.
+//     Методи:
+// changePassword(newPassword) — змінює пароль.
+//     Гетер info — повертає рядок: "ID: [id], Name: [name], Email: [email]".
+//     Додатково:
+//
+// Використовуй статичну властивість для автоматичного створення id.
+class User {
+    #id = 1;
+    name = null;
+    email = null;
+    #password = null;
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
