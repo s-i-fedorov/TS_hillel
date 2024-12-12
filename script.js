86bbf3393072be5944e2d5ca04f96c93b4a038a5
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-12-12 23:54:23 +0200

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict'
-new class EventManager {
+class EventManager {
     handlers = {}
 
     on(eName, eHandler){
@@ -12,7 +12,7 @@ new class EventManager {
     off(eName, eHandler){
         if(!this.handlers[eName]) return;
 
-        const index = this.handlers.findIndex((existingHandler)=>{
+        const index = this.handlers[eName].findIndex((existingHandler)=>{
             return existingHandler === eHandler
         })
 
@@ -24,5 +24,17 @@ new class EventManager {
             handler(eName, this)
         })
     }
-
 }
+const handler1 = (name,e)=>{
+    console.log('handler1',name,e)}
+const handler2 = (name,e)=>{
+    console.log('handler2',name,e)}
+const handler3 = (name,e)=>{
+    console.log('handler3',name,e)}
+const testMgr = new EventManager()
+testMgr.on('hello',handler1)
+testMgr.on('hello',handler2)
+testMgr.on('hello',handler3)
+// console.log(testMgr);
+testMgr.off('hello',handler2)
+testMgr.trigger('hello')
