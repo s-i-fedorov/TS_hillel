48d6fb5ff0da8f5ff00983d0a9e2a3ba5d9c919a
s-i-fedorov <s.i.fedorov.87@gmail.com>
2024-12-17 22:45:06 +0200

    in process

--- src/classes/user.js
+++ src/classes/user.js
@@ -1,18 +1,19 @@
 'use strict'
 export default class User {
-    static id = 0;
+    static #id = 0;
     name = null;
     email = null;
     #password = null;
     constructor(name, email) {
-        this.id = User.id
-        User.id+=1
+        this.id = User.#id
+        User.#id+=1
         this.name = name;
         this.email = email;
     }
 
     set changePassword(pass){
-        this.#password = pass;
+        if(typeof(pass)!== 'string') return;
+        this.#password = atob(pass);
     }
     get info(){
         return `ID: ${this.id}, Name: ${this.name}, Email: ${this.email}`
